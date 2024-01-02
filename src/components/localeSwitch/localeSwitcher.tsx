b30dde8d19fd22f234784cab7e8b3f39b28db35e
Arthur Bernard <arthurbernardpereira@gmail.com>
2024-01-02 12:04:26 -0300

    Style: language switcher formated

--- src/components/localeSwitch/localeSwitcher.tsx
+++ src/components/localeSwitch/localeSwitcher.tsx
@@ -1,14 +1,18 @@
 import { locales, Link } from '@/navigation';
 import LocaleSwitcherSelect from './localeSwitcherSelect';
 import styles from './localeSwitcher.module.css'
+import { useLocale } from 'next-intl';
 
 
 export default function LocaleSwitcher() {
   const langs =locales;
 
+  const locale = useLocale(); // Use useLocale to get the current locale
+  
+
   return (
     <div className={styles.switcher}>
-    <LocaleSwitcherSelect langs={langs}/>
+    <LocaleSwitcherSelect langs={langs} lang={locale}/>
     </div>
   );
 }
--- src/components/localeSwitch/localeSwitcherSelect.tsx
+++ src/components/localeSwitch/localeSwitcherSelect.tsx
@@ -1,40 +1,68 @@
 'use client';
-import { useTransition } from 'react';
+import { useEffect, useState, useTransition } from 'react';
 import { useRouter, usePathname, Link } from '@/navigation';
 import styles from './localeSwitcher.module.css'
+import React from 'react';
 
 type Props = {
   langs: any;
+  lang: string
 };
 export default function LocaleSwitcherSelect({
   langs,
+  lang,
 
 }: Props) {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
   const pathname = usePathname();
+  const [scrolling, setScrolling] = useState<boolean>(false);
+  const [lastScrollPosition, setLastScrollPosition] = useState<number>(0);
+
 
   const handleLinkClick = (nextLocale: string) => {
     startTransition(() => {
       router.replace(pathname, { locale: nextLocale });
     });
   };
+  const handleScroll = () => {
+    const currentScrollPosition: number = window.scrollY;
+    setScrolling(
+      lastScrollPosition > currentScrollPosition && currentScrollPosition !== 0
+    );
+    setLastScrollPosition(currentScrollPosition);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollPosition]);
 
   return (
-    <div>
-      {langs.map((cur: any) => (
-        <Link
-          key={cur}
-          locale={cur}
-          href={pathname}
-          scroll={false}
-          onClick={(event) => {
-            handleLinkClick(cur);
-          }}
-          >
-          {cur}
-        </Link>
-      ))}
+    <div className={`${scrolling? styles.switcher1:styles.switcher2}`}>
+      <div  aria-current={scrolling
+        ? true : false}
+        className={`${styles.switch} ${lang === 'en'
+          ? styles.switch1 : styles.switch2}
+           ${scrolling
+            ? styles.scrolledSwitch : ''}`} >
+        {langs.map((cur: any) => (
+          <>
+            {isPending ? (
+              <span className={styles.disabledLink}>{cur}</span>
+            ) : (
+              <Link locale={cur} href={pathname} scroll={false} onClick={() => handleLinkClick(cur)}>
+
+                {cur}
+
+              </Link>
+            )}
+          </>
+        ))}
+      </div>
     </div>
-  )
+
+  );
 }
